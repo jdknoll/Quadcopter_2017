e05d00061e3b7e08d48d3e6d13b1fc60e43fd9f0
Scott Keller <kellerboy.823@gmail.com>
2017-03-31 19:53:39 -0400

    Enabled peripherals for PB as well as PC and PD but commented them out as they are enabled in ultrasonic

--- system_clock_set.c
+++ system_clock_set.c
@@ -25,8 +25,4 @@ void clock_config()
 {
        //Setting the Clock
 	   ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_XTAL_16MHZ);
-
-       //Enabling the peripherals used by the program (F,C,D)
-       ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-       ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1); // The Tiva C Launchpad has two modules (0 and 1). Module 1 covers the LED pins
 }
