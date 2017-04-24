583fe17f06e0a065b1ca87896622ea432c403deb
jdknoll <jk1135@cs.ship.edu>
2017-04-24 18:04:59 -0400

    committing the quaternion filter - not compileable

---
+++ accelerometer/tiva_uno.cpp
@@ -0,0 +1,27 @@
+/*
+ * tiva_uno.cpp
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: Josiah Knoll
+ *
+ *  This function creates the wire and serial libraries that the cpp expects
+ */
+#include <stdint.h>
+
+
+// Initiate the Wire library and join the I2C bus as a master or slave. This should normally be called only once.
+void Wire::begin(uint8_t address){
+
+}
+void Wire::beginTransmission(uint8_t address){
+
+}
+void Wire::endTransmission(){
+
+}
+void Wire::write(){
+
+}
+void Wire::requestFrom(){
+
+}
